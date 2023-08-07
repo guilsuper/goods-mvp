5e7ead9b6c11887e9d059ec21b7ca06a4336c247
“guilsuper” <gm.may.power@gmail.com>
2023-08-07 09:40:17 -0400

    allow one to override the EMAIL_HOST via an environment variable

--- backend/product_mvp/settings.py
+++ backend/product_mvp/settings.py
@@ -181,7 +181,7 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_FROM = os.environ["EMAIL_USER"]
