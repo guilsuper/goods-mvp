b169e57c871253b838b68a958abd65be72ea55a6
“guilsuper” <gm.may.power@gmail.com>
2023-08-17 09:06:47 -0700

    fixed typo issue

--- backend/api/tests/factories/factories.py
+++ backend/api/tests/factories/factories.py
@@ -58,7 +58,7 @@ class AdministratorFactory(DjangoModelFactory):
         # DjangoModelFactory in future will stop issuing
         # a second call to save() on the created instance when
         # Post-generation hooks return a value (method 'group')
-        # Appropriet warning is displayed if this setting is missing
+        # Appropriate warning is displayed if this setting is missing
         # according to the official changeLog 3.3.0 (2023-07-19)
         # https://factoryboy.readthedocs.io/en/stable/changelog.html
         skip_postgeneration_save = True
