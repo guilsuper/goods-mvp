42c9bf598edd4bf6223d2ad88f7ad6e776aa7648
“guilsuper” <gm.may.power@gmail.com>
2023-08-19 05:46:23 -0300

    freedomhouse: starting a python module for accessing the freedomhouse scores, statuses, and soon map images

---
+++ backend/freedomhouse/__init__.py
@@ -0,0 +1,43 @@
+# Copyright 2023 Free World Certified -- all rights reserved.
+'''Interface to key info from freedomhouse.org see README.md
+
+'''
+import csv
+from collections import namedtuple
+
+FreedomHouseRecord = namedtuple(
+    'FreedomHouseRecord',
+    'name,region,country_or_territory,status,score')
+
+places = {}
+
+with open('freedomhouse.csv', 'r') as filehandle:
+    for fhr in map(FreedomHouseRecord._make, csv.reader(filehandle)):
+        places[fhr.name] = fhr
+
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('place_name')
+    args = parser.parse_args()
+
+    if args.place_name not in places:
+        all_names = ', '.join(sorted(places.keys()))
+        sys.exit(f'The name "{args.place_name}" is not in {all_names}')
+
+    else:
+        fhr = places[args.place_name]
+        c_or_t = (fhr.country_or_territory == "c" and "Country" or "Territory")
+        if fhr.status == 'NF':
+            freedom_status = 'not free.'
+        elif fhr.status == 'PF':
+            freedom_status = 'partially free.'
+        else:
+            freedom_status = 'FREE!'
+
+        print(f'{fhr.name} is a {c_or_t} in {fhr.region} that '
+              f'scored {fhr.score} out of 100 for data from 2022, '
+              f'which makes it {freedom_status}')
