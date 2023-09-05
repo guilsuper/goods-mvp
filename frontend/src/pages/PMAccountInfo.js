0f241b9306d7f51604dfd2b6b6b40c9764596f83
“guilsuper” <gm.may.power@gmail.com>
2023-09-05 12:14:12 -0400

    spell out abbreviation PM as Product Manager

--- frontend/src/pages/PMAccountInfo.js
+++ frontend/src/pages/PMAccountInfo.js
@@ -49,7 +49,7 @@ const PMAccountInfo = () => {
   }
 
   const deletePM = async (event) => {
-    if (!window.confirm('Are you sure you want to permanently delete this PM?')) {
+    if (!window.confirm('Are you sure you want to permanently delete this Product Manager?')) {
       return
     }
 
@@ -87,11 +87,11 @@ const PMAccountInfo = () => {
 
   return (
     <Container>
-      <h3 className="text-center">PM information</h3>
+      <h3 className="text-center">Product Manager information</h3>
       <Col className="p-5 mb-5 mx-auto w-75 rounded shadow">
-        <Row className="text-secondary"><p>User First Name</p></Row>
+        <Row className="text-secondary"><p>First Name</p></Row>
         <Row><p>{pm.first_name}</p></Row>
-        <Row className="text-secondary"><p>User Last Name</p></Row>
+        <Row className="text-secondary"><p>Last Name</p></Row>
         <Row><p>{pm.last_name}</p></Row>
         <Row className="text-secondary"><p>Email</p></Row>
         <Row><p>{pm.email}</p></Row>
@@ -105,7 +105,7 @@ const PMAccountInfo = () => {
               <Button variant="primary" as={Link} to={'/account/pm/edit/' + pm.email}>Edit</Button>
             </Col>
             <Col md={{ span: 4, offset: 4 }}>
-              <Button variant="danger" onClick={deletePM}>Delete PM</Button>
+              <Button variant="danger" onClick={deletePM}>Delete</Button>
             </Col>
           </Row>
             : ' '
