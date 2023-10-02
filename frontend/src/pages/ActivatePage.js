979fafbbfd59d6738c26b444e73a55ad1f96cb41
“guilsuper” <gm.may.power@gmail.com>
2023-10-02 17:09:23 -0400

    ensure that activation is never activated twice

--- frontend/src/pages/ActivatePage.js
+++ frontend/src/pages/ActivatePage.js
@@ -2,29 +2,30 @@
  * Copyright 2023 Free World Certified -- all rights reserved.
  */
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Container } from 'react-bootstrap'
 import { useNavigate, useParams } from 'react-router'
 
 const ActivatePage = () => {
   const { uidb64, token } = useParams()
   const [message, setMessage] = useState([])
+  const activatedAlready = useRef(false)
   const navigate = useNavigate()
 
   useEffect(() => {
     async function activate () {
-      let response = ''
-      try {
-        response = await fetch('/api/activate/' + uidb64 + '/' + token + '/')
-      } catch (error) {
-        alert('Server is not responding')
-        return
-      }
-
-      if (response.status === 200) {
-        setMessage('Successfully activated!')
-      } else {
-        navigate('/sign-up')
+      if (!activatedAlready.current) {
+        try {
+          activatedAlready.current = true
+          const response = await fetch('/api/activate/' + uidb64 + '/' + token + '/')
+          if (response.status === 200) {
+            setMessage('Successfully activated!')
+          } else {
+            navigate('/sign-up')
+          }
+        } catch (error) {
+          alert('Server is not responding')
+        }
       }
     }
     activate()
