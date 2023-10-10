c809f3c2befcfd18d2ff698c3a0e737bb71ea8ae
“guilsuper” <gm.may.power@gmail.com>
2023-10-10 17:49:28 -0400

    remove newly introduced space

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
