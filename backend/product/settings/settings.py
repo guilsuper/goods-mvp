83c9be01808aa71895d17cd87bfbc2988b017b43
“guilsuper” <gm.may.power@gmail.com>
2023-10-23 09:27:53 -0400

    Revert "fix typo day->days"
    This reverts commit e2d3b0b38c7107b0da803bbceb1c7fc0777c187a.

--- backend/product/settings/settings.py
+++ backend/product/settings/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(day=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
